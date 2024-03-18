ecac8b4aa4a05b8e1f63c913ae3ec4bfd3e59284
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-18 14:49:46 -0400

    Test prototype

--- src/prototype.js
+++ src/prototype.js
@@ -1,28 +1,7 @@
-const parent = {
-    value: 2,
-    method() {
-        return this.value + 1;
-    },
-};
-
-console.log(parent.method()); // 3
-// When calling parent.method in this case, 'this' refers to parent
-
-// child is an object that inherits from parent
-const child = {
-    __proto__: parent,
-};
-console.log(child.method()); // 3
-// When child.method is called, 'this' refers to child.
-// So when child inherits the method of parent,
-// The property 'value' is sought on child. However, since child
-// doesn't have an own property called 'value', the property is
-// found on the [[Prototype]], which is parent.value.
-
-child.value = 4; // assign the value 4 to the property 'value' on child.
-// This shadows the 'value' property on parent.
-// The child object now looks like:
-// { value: 4, __proto__: { value: 2, method: [Function] } }
-console.log(child.method()); // 5
-// Since child now has the 'value' property, 'this.value' means
-// child.value instead
+class doSomething {
+    constructor() {}
+}
+doSomething.prototype.foo = "bar";
+const doSomethingInstance = new doSomething();
+doSomethingInstance.prop = "test";
+console.log(doSomethingInstance);
